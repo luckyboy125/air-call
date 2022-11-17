948656e904984414f0933dae689c34cc733dd555
luckyboy125 <luckyboy.ws125@gmail.com>
2022-11-17 23:32:45 +0200

    content item style css is finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,30 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <div className="container-view">Some activities should be here</div>
+      <div className="select-all-archive">
+        <span>
+          <i className="fal fa-archive"></i>
+          Archive all calls
+        </span>
+      </div>
+      <div className="call-item-divline">
+        <div className="call-dottedline"></div>
+        <span>JULY, 27 2017</span>
+        <div className="call-dottedline"></div>
+      </div>
+      <div className="call-item">
+        <div className="first-part">
+          <i className="fas fa-phone-alt phone-icon">
+            <i className="fas fa-arrow-down status"></i>
+          </i>
+          <div>
+            <div className="phone-number">+33 6 45 12 53 91</div>
+            <div className="phone-des">tried to call on Xanier</div>
+          </div>
+        </div>
+        <div className="last-part"></div>
+        <div></div>
+      </div>
     </div>
   );
 }
